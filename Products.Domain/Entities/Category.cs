c491f3f0995d330286e3255d50207984e85f30e4
cmedrano <cristian.ariel.medrano@gmail.com>
2025-11-17 10:03:20 -0300

    add categories controller

--- Products.Domain/Entities/Category.cs
+++ Products.Domain/Entities/Category.cs
@@ -4,6 +4,11 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = null!;
+
+        public Category(string name)
+        {
+            Name = name;
+        }
         public ICollection<ProductCategory> ProductCategories { get; set; } = new List<ProductCategory>();
     }
 }
